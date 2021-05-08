1c617bdade1551f8e6dc537cbe030914d5522944
Burnett <00burnettghartey@gmail.com>
2021-05-08 11:25:43 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/signup.html');
+});
+app.post('/', function(req, res){
+    var firstname = req.body.fName;
+    var lastname = req.body.lName;
+    var email = req.body.email;
+
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstname,
+                    LNAME: lastname
+                }
+            }
+        ]
+    }
+    var jsonData = JSON.stringify(data);
+
+    var options = {
+        url: "https://us1.api.mailchimp.com/3.0/lists/4798439246?skip_merge_validation=true&skip_duplicate_check=true",
+        method : 'POST',
+        headers: {
+            "Authorization": "Burnett1 859055d9736dd172f695c5cfe61ba9ce-us1"
+        },
+        body: jsonData
+    }
+    request(options, function(error, response, body){
+        if(error){
+            res.sendFile(__dirname + '/failure.html')
+        }else{
+            if(response.statusCode == 200){
+                res.sendFile(__dirname + '/success.html')
+            }else{
+                res.sendFile(__dirname + '/failure.html');
+            }
+        }
+    });
+});
+
+app.post('/failure', function(req, res){
+    res.redirect('/');
+})
+app.listen(process.env.PORT || 3000, function(){
+    console.log('server running on port 3000');
+});
